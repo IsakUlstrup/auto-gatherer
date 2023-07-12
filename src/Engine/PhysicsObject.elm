1d72232743f9f57c6cf9cc6998a796a0d62e2737
Isak <isakulstrup@gmail.com>
2023-07-12 14:50:58 +0200

    fixed friction bug

--- src/Engine/PhysicsObject.elm
+++ src/Engine/PhysicsObject.elm
@@ -63,7 +63,7 @@ applyFriciton friction object =
     let
         adjustedFriction : Float
         adjustedFriction =
-            clamp 0 1 friction |> (\x -> x - 1)
+            clamp 0 1 friction |> (\x -> 1 - x)
     in
     { object | velocity = Vector2.scale adjustedFriction object.velocity }
 
