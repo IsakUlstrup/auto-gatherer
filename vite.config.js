a2f7ad4d9c83775575bef4f76346d643d5c833b3
Isak <isakulstrup@gmail.com>
2023-07-22 19:46:56 +0200

    added elm-optimize-level-2

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,12 @@ import { defineConfig } from 'vite'
 import elmPlugin from 'vite-plugin-elm'
 
 export default defineConfig({
-    plugins: [elmPlugin()]
+    plugins: [elmPlugin({
+        optimize: false, // no `--optimize` option when using elm-optimize-level-2
+        nodeElmCompilerOptions: {
+            pathToElm: process.env.NODE_ENV === 'production' ? 'node_modules/elm-optimize-level-2/bin/elm-optimize-level-2' : undefined
+        }
+    })]
 })
+
+
