4ce38826c777ec79d45840976d13360ef8e5d927
Isak <isakulstrup@gmail.com>
2023-08-14 15:39:19 +0200

    added some preset particles

--- src/Content/Worlds.elm
+++ src/Content/Worlds.elm
@@ -1,6 +1,7 @@
 module Content.Worlds exposing (testWorld1)
 
 import Content.ParticleState exposing (ParticleState(..))
+import Engine.Vector2 as Vector2
 import Engine.World as World exposing (World)
 
 
@@ -11,9 +12,14 @@ addWall x y r =
     World.addFixedParticle x y r Idle
 
 
+addMoveToPosition : Float -> Float -> World ParticleState -> World ParticleState
+addMoveToPosition x y =
+    World.addStaticParticle x y 20 0.05 (MoveToPosition <| Vector2.new x y)
+
+
 testWorld1 : World ParticleState
 testWorld1 =
-    World.new (MoveAwayAngle False 0) 0.2
+    World.new Idle 0.2
         |> World.addDynamicParticle 0 80 50 0 Idle
         |> World.addStaticParticle 200 20 15 0.1 (FollowId 1)
         |> World.addStaticParticle 220 30 15 0.1 (FollowId 2)
@@ -23,31 +29,37 @@ testWorld1 =
         -- |> World.addDynamicParticle -97 20 15 0.08 MoveToClosest
         -- |> World.addDynamicParticle 200 20 40 0.08 MoveToCenter
         |> World.addDynamicParticle -300 200 55 0 Idle
-        -- |> World.addDynamicParticle -100 20 20 0.1 MoveToCenter
-        -- |> World.addDynamicParticle -101 20 20 0.12 MoveToCenter
-        -- |> World.addDynamicParticle -102 20 20 0.13 MoveToCenter
-        -- |> World.addDynamicParticle -103 20 20 0.14 MoveToCenter
-        -- |> World.addDynamicParticle -104 20 20 0.15 MoveToCenter
+        |> World.addDynamicParticle -100 20 20 0.1 MoveToCenter
+        |> World.addDynamicParticle -101 20 20 0.12 MoveToCenter
+        |> World.addDynamicParticle -102 20 20 0.13 MoveToCenter
+        |> World.addDynamicParticle -103 20 20 0.14 MoveToCenter
+        |> World.addDynamicParticle -104 20 20 0.15 MoveToCenter
         -- |> World.addDynamicParticle -150 20 20 MoveToClosest
         -- |> World.addDynamicParticle -150 50 20 MoveToClosest
         -- |> World.addDynamicParticle 150 20 12 0.08 MoveToClosest
-        -- |> addTree 200 0 500 -73
-        -- |> World.addStaticParticle -100 -100 20 0.05 (MoveToPosition <| Vector2.new 460 -80)
-        -- |> World.addStaticParticle 100 100 20 0.05 (MoveToPosition <| Vector2.new 450 -75)
-        -- |> World.addStaticParticle 300 -100 20 0.05 (MoveToPosition <| Vector2.new 440 -65)
-        -- |> World.addStaticParticle 350 -120 20 0.05 (MoveToPosition <| Vector2.new 455 -55)
-        -- |> World.addStaticParticle 350 120 20 0.05 (MoveToPosition <| Vector2.new 440 -35)
-        |> World.addStaticParticle 180 100 15 0.1 (FollowMoveToPosition 150)
-        |> World.addStaticParticle 140 -107 14 0.1 (FollowMoveToPosition 150)
-        |> World.addStaticParticle -107 12 13 0.1 (FollowMoveToPosition 150)
-        |> World.addStaticParticle -240 -107 12 0.09 (FollowMoveToPosition 150)
-        |> World.addStaticParticle -241 -97 12 0.08 (FollowMoveToPosition 100)
-        |> World.addStaticParticle -252 -117 12 0.08 (FollowMoveToPosition 100)
-        |> World.addStaticParticle -263 -137 12 0.07 (FollowMoveToPosition 100)
-        |> World.addStaticParticle -274 -87 12 0.06 (FollowMoveToPosition 100)
-        |> World.addStaticParticle -285 -117 12 0.05 (FollowMoveToPosition 100)
-        |> World.addStaticParticle -296 -77 12 0.07 (FollowMoveToPosition 100)
-        |> World.addStaticParticle -207 -107 12 0.05 (FollowMoveToPosition 100)
+        |> addMoveToPosition 310 -100
+        |> addMoveToPosition 300 -120
+        |> addMoveToPosition 300 -100
+        |> addMoveToPosition 350 -120
+        |> addMoveToPosition 350 -120
+        |> addMoveToPosition 340 -114
+        |> addMoveToPosition 330 -100
+        |> addMoveToPosition 328 -124
+        |> addMoveToPosition 298 -99
+        |> addMoveToPosition 288 -103
+        |> addMoveToPosition 278 -113
+        |> addMoveToPosition 268 -93
+        -- |> World.addStaticParticle 180 100 15 0.1 (FollowMoveToPosition 150)
+        -- |> World.addStaticParticle 140 -107 14 0.1 (FollowMoveToPosition 150)
+        -- |> World.addStaticParticle -107 12 13 0.1 (FollowMoveToPosition 150)
+        -- |> World.addStaticParticle -240 -107 12 0.09 (FollowMoveToPosition 150)
+        -- |> World.addStaticParticle -241 -97 12 0.08 (FollowMoveToPosition 100)
+        -- |> World.addStaticParticle -252 -117 12 0.08 (FollowMoveToPosition 100)
+        -- |> World.addStaticParticle -263 -137 12 0.07 (FollowMoveToPosition 100)
+        -- |> World.addStaticParticle -274 -87 12 0.06 (FollowMoveToPosition 100)
+        -- |> World.addStaticParticle -285 -117 12 0.05 (FollowMoveToPosition 100)
+        -- |> World.addStaticParticle -296 -77 12 0.07 (FollowMoveToPosition 100)
+        -- |> World.addStaticParticle -207 -107 12 0.05 (FollowMoveToPosition 100)
         -- |> World.addStaticParticle -248 -107 12 FollowMoveToPosition
         |> World.addDynamicParticle -340 -107 23 0.06 Avoid
         |> World.addDynamicParticle 240 -17 18 0.05 Avoid
@@ -70,11 +82,8 @@ testWorld1 =
         |> addWall -527 20 92
         |> addWall -727 -197 92
         |> addWall -727 10 92
-
-
-
--- |> World.addDynamicParticle 0 200 20 0.1 Meander
--- |> World.addDynamicParticle 100 200 20 0.1 Meander
--- |> World.addDynamicParticle 200 200 20 0.1 Meander
--- |> World.addDynamicParticle 300 200 20 0.1 Meander
--- |> World.addDynamicParticle 400 200 20 0.1 Meander
+        |> World.addDynamicParticle 0 200 20 0.1 Meander
+        |> World.addDynamicParticle 100 200 20 0.1 Meander
+        |> World.addDynamicParticle 200 200 20 0.1 Meander
+        |> World.addDynamicParticle 300 200 20 0.1 Meander
+        |> World.addDynamicParticle 400 200 20 0.1 Meander
